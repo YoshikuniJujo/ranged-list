336f2cb61fb7f825f434c2d30fd737ab224d3789
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-01-27 14:05:08 +0900

    Data.List.Length: refactor body

--- memo.md
+++ memo.md
@@ -211,14 +211,14 @@ refactor
 		- [x] TYPE
 		- [x] UNFOLDL
 		- [x] LIST TO LENGTH RIGHT
-* [ ] refactor Data.List.Length
-	+ [ ] language extension
-	+ [ ] export list
-	+ [ ] import list
-	+ [ ] structure
-	+ [ ] body
-		- [ ] LENGTH LEFT
-		- [ ] LENGTH RIGHT
+* [x] refactor Data.List.Length
+	+ [x] language extension
+	+ [x] export list
+	+ [x] import list
+	+ [x] structure
+	+ [x] body
+		- [x] LENGTH LEFT
+		- [x] LENGTH RIGHT
 * [ ] refactor Data.List.Range.Nat
 	+ [ ] language extension
 	+ [ ] export list
--- src/Data/List/Length.hs
+++ src/Data/List/Length.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BlockArguments, TupleSections #-}
 {-# LANGUAGE DataKinds, TypeOperators #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -43,7 +43,7 @@ module Data.List.Length (
 	RightToLeft, (++..), rightToLeft ) where
 
 import GHC.TypeNats (type (-))
-import Control.Arrow (first)
+import Control.Arrow (first, (***))
 import Data.List.Range (
 	RangeL(..), AddL, (++.), LoosenLMax, loosenLMax, Unfoldr,
 	ZipL, zipL, zipWithL, zipWithML,
@@ -73,15 +73,12 @@ fillL :: Unfoldr n m m => RangeL n m a -> a -> LengthL m a
 fillL = (`unfoldrWithBase` \x -> (x, x))
 
 chunksL :: ListToLengthL n => [a] -> ([LengthL n a], RangeL 0 (n - 1) a)
-chunksL xs = case listToLengthL xs of
-	Left ys -> ([], ys)
-	Right (ys, xs') -> (ys :) `first` chunksL xs'
+chunksL = either ([] ,) (uncurry first . ((:) *** chunksL)) . listToLengthL
 
 chunksL' :: (Unfoldr 0 n n, LoosenLMax 0 (n - 1) n, ListToLengthL n) =>
 	a -> [a] -> [LengthL n a]
 chunksL' z xs = case chunksL xs of
-	(cs, NilL) -> cs
-	(cs, rs) -> cs ++ [loosenLMax rs `fillL` z]
+	(cs, NilL) -> cs; (cs, rs) -> cs ++ [loosenLMax rs `fillL` z]
 
 ---------------------------------------------------------------------------
 -- LENGTH RIGHT
@@ -94,12 +91,9 @@ fillR :: Unfoldl n m m => a -> RangeR n m a -> LengthR m a
 fillR = unfoldlWithBase \x -> (x, x)
 
 chunksR :: ListToLengthR n => [a] -> ([LengthR n a], RangeR 0 (n - 1) a)
-chunksR xs = case listToLengthR xs of
-	Left ys -> ([], ys)
-	Right (ys, xs') -> (ys :) `first` chunksR xs'
+chunksR = either ([] ,) (uncurry first . ((:) *** chunksR)) . listToLengthR
 
 chunksR' :: (Unfoldl 0 n n, LoosenRMax 0 (n - 1) n, ListToLengthR n) =>
 	a -> [a] -> [LengthR n a]
 chunksR' z xs = case chunksR xs of
-	(cs, NilR) -> cs
-	(cs, rs) -> cs ++ [z `fillR` loosenRMax rs]
+	(cs, NilR) -> cs; (cs, rs) -> cs ++ [z `fillR` loosenRMax rs]
