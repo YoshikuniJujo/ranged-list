1fea73b730404569d2d13b26e9a69555472662d0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-02-12 15:36:07 +0900

    sample/fourPointOfRect.hs

--- sample/fourPointsOfRect.hs
+++ sample/fourPointsOfRect.hs
@@ -6,21 +6,43 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs -fplugin=Plugin.TypeCheck.Nat.Simple #-}
 
 import GHC.TypeNats
+import Control.Monad.Catch
 import Data.List.Length
 
 main :: IO ()
 main = do
-	r <- getElems @0 @4 NilR do
-		l <- getLine
-		case l of
-			"d" -> pure $ Just Delete
-			_ -> pure . Just $ Value l
+	r <- tryGetting
 	print r
+	loop r
+	where
+	loop :: LengthR 4 String -> IO ()
+	loop xa@(xs :+ _) = getLine >>= \case
+		"q" -> pure ()
+		"d" -> do
+			r <- (getElems @3 @1 xs $ getLine >>= \case
+				"d" -> pure $ Just Delete
+				l -> pure . Just $ Value l) `catch` \(_ :: NothingToDeleteException) -> do
+					putStrLn "*** Nothing to delete."
+					tryGetting
+			print r
+			loop r
+		_ -> putStrLn "q or d" >> loop xa
+
+tryGetting :: IO (LengthR 4 String)
+tryGetting = getElems NilR
+	(getLine >>=
+		\case "d" -> pure $ Just Delete; l -> pure . Just $ Value l)
+	`catch`
+	\(_ :: NothingToDeleteException) -> do
+		putStrLn "*** Nothing to delete."
+		tryGetting
 
 data DeleteOr a = Delete | Value a deriving Show
+data NothingToDeleteException = NothingToDeleteException deriving Show
+instance Exception NothingToDeleteException
 
 class GetElems n v where
-	getElems :: MonadFail m =>
+	getElems :: MonadThrow m =>
 		LengthR n a -> m (Maybe (DeleteOr a)) -> m (LengthR (n + v) a)
 
 instance GetElems 0 0 where getElems NilR _ = pure NilR
@@ -30,7 +52,7 @@ instance 1 <= n => GetElems n 0 where getElems xs@(_ :+ _) _ = pure xs
 instance GetElems 1 (v - 1) => GetElems 0 v where
 	getElems NilR gt = gt >>= \case
 		Nothing -> getElems NilR gt
-		Just Delete -> fail "Nothing to delete."
+		Just Delete -> throwM NothingToDeleteException
 		Just (Value x) -> getElems @1 @(v - 1) (NilR :+ x) gt
 
 instance {-# OVERLAPPABLE #-}
