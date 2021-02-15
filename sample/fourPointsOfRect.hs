9880859bbc06631803284b53328cd569fa2955a5
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-02-15 09:37:49 +0900

    sample/fourPointsOfRect.hs: rearrange

--- sample/fourPointsOfRect.hs
+++ sample/fourPointsOfRect.hs
@@ -12,36 +12,6 @@ import Control.Monad.Catch
 import Data.List.Length
 import Text.Read
 
-main :: IO ()
-main = tryGetting NilR >>= fix \go xa@(xs :+ _) -> getLine >>= \case
-	"q" -> pure ();
-	"d" -> go =<< tryGetting xs
-	_ -> putStrLn "q or d" >> go xa
-
-tryGetting :: forall n . (GetElems n (4 - n), GetElems 0 4) => LengthR n Double -> IO (LengthR (0 + 4) Double)
-tryGetting xs = (<$) <$> id <*> printResult =<< getElems @n @(4 - n) xs
-	(getLine >>=
-		\case "d" -> pure $ Just Delete; l -> pure (Value <$> readMaybe l))
-	`catch`
-	\(_ :: NothingToDeleteException) -> do
-		putStrLn "*** Nothing to delete."
-		tryGetting @0 (NilR :: LengthR 0 Double)
-
-printResult :: LengthR 4 Double -> IO ()
-printResult r = do
-	putStrLn ""
-	putStrLn `mapM_` withTitles 6 (NilR :+ "left" :+ "top" :+ "width" :+ "height") r
-	putStrLn ""
-	putStrLn `mapM_` withTitles 12 (NilR :+ "left-top" :+ "right-top" :+ "left-bottom" :+ "right-bottom") (fourPoints r)
-	putStrLn ""
-
-withTitles :: (Show a, Applicative (LengthR n)) => Int -> LengthR n String -> LengthR n a -> LengthR n String
-withTitles n ts xs = (\t v -> t ++ replicate (n - length t) ' ' ++ ": " ++ show v) <$> ts <*> xs
-
-fourPoints :: LengthR 4 Double -> LengthR 4 (Double, Double)
-fourPoints (NilR :+ l :+ t :+ w :+ h) =
-	NilR :+ (l, t) :+ (l + w, t) :+ (l, t + h) :+ (l + w, t + h)
-
 data DeleteOr a = Delete | Value a deriving Show
 data NothingToDeleteException = NothingToDeleteException deriving Show
 instance Exception NothingToDeleteException
@@ -66,3 +36,33 @@ instance {-# OVERLAPPABLE #-}
 		Nothing -> getElems xa gt
 		Just Delete -> getElems @(n - 1) @(v + 1) xs gt
 		Just (Value x) -> getElems @(n + 1) @(v - 1) (xa :+ x) gt
+
+tryGetting :: forall n . (GetElems n (4 - n), GetElems 0 4) => LengthR n Double -> IO (LengthR (0 + 4) Double)
+tryGetting xs = (<$) <$> id <*> printResult =<< getElems @n @(4 - n) xs
+	(getLine >>=
+		\case "d" -> pure $ Just Delete; l -> pure (Value <$> readMaybe l))
+	`catch`
+	\(_ :: NothingToDeleteException) -> do
+		putStrLn "*** Nothing to delete."
+		tryGetting @0 (NilR :: LengthR 0 Double)
+
+withTitles :: (Show a, Applicative (LengthR n)) => Int -> LengthR n String -> LengthR n a -> LengthR n String
+withTitles n ts xs = (\t v -> t ++ replicate (n - length t) ' ' ++ ": " ++ show v) <$> ts <*> xs
+
+fourPoints :: LengthR 4 Double -> LengthR 4 (Double, Double)
+fourPoints (NilR :+ l :+ t :+ w :+ h) =
+	NilR :+ (l, t) :+ (l + w, t) :+ (l, t + h) :+ (l + w, t + h)
+
+printResult :: LengthR 4 Double -> IO ()
+printResult r = do
+	putStrLn ""
+	putStrLn `mapM_` withTitles 6 (NilR :+ "left" :+ "top" :+ "width" :+ "height") r
+	putStrLn ""
+	putStrLn `mapM_` withTitles 12 (NilR :+ "left-top" :+ "right-top" :+ "left-bottom" :+ "right-bottom") (fourPoints r)
+	putStrLn ""
+
+main :: IO ()
+main = tryGetting NilR >>= fix \go xa@(xs :+ _) -> getLine >>= \case
+	"q" -> pure ();
+	"d" -> go =<< tryGetting xs
+	_ -> putStrLn "q or d" >> go xa
